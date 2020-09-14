a507ae57e59b61ebaa34362be024d6bdd364b43c
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2020-09-14 10:30:35 +0200

    change sportsdata id

--- Tippspiel.Implementation/Configs/SportdataConfigInfo.cs
+++ Tippspiel.Implementation/Configs/SportdataConfigInfo.cs
@@ -11,9 +11,9 @@
         public SportsdataConfigInfo()
         {
             LeagueShortcut = "bl1";
-            LeagueSaison = "2019";
-            StartSpieltag = 18;
-            EndSpieltag = 34;
+            LeagueSaison = "2020";
+            StartSpieltag = 1;
+            EndSpieltag = 17;
         }
     }
 }
