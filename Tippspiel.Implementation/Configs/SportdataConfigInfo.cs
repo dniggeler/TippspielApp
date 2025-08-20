ae010fdabd1bb4a43b250be29cd66cd84982a19e
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2025-08-20 18:26:06 +0200

    update for season 2025 I

--- Tippspiel.Implementation/Configs/SportdataConfigInfo.cs
+++ Tippspiel.Implementation/Configs/SportdataConfigInfo.cs
@@ -3,10 +3,10 @@
     public class SportsdataConfigInfo : AppInfo<SportsdataConfigInfo>
     {
         // Addition shared info
-        public int LeagueId { get; set; } = 4741;
+        public int LeagueId { get; set; } = 4821;
         public string LeagueShortcut { get; set; } = "bl1";
-        public string LeagueSaison { get; set; } = "2024";
-        public int StartSpieltag { get; set; } = 18;
-        public int EndSpieltag { get; set; } = 34;
+        public string LeagueSaison { get; set; } = "2025";
+        public int StartSpieltag { get; set; } = 1;
+        public int EndSpieltag { get; set; } = 17;
     }
 }
